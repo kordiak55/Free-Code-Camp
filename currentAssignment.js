ca32e0ed7502438140e483e1e702c0b192241aab
Mike <kordiak@gmail.com>
2018-02-16 14:18:27 -0500

    push

--- currentAssignment.js
+++ currentAssignment.js
@@ -16,20 +16,30 @@ function permAlone(str) {
 
     var totalPermutations = factorial(str.length);
     var anchorPermutations = totalPermutations/str.length;
+    var strArr = str.split('');
 
     for(i=0;i<str.length;i++){
 
+        var strCut = strArr.slice(0);
+        
+        let anchor = strCut.splice(i,1);
+        //console.log(anchor);
+        //console.log(strCut);
+
         for(j=0;j<anchorPermutations;j++){
          
-            
-
+            let jumper = strCut.splice(0,1);  
+            strCut.push(jumper);
+            strCut.unshift(anchor);
+            console.log(JSON.stringify(strCut));
             
         }
 
     }
 
+    var permutations = 1;
     return permutations;
   }
   
-  console.log(permAlone('aab'));
+  console.log(permAlone('abc'));
   
